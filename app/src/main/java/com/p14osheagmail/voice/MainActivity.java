92bb2fd54d4439671822b66ec5173bc64e9c07d3
Patrick O'Shea <p14oshea@gmail.com>
2017-09-27 23:31:20 +0100

    second commit

--- app/src/main/java/com/p14osheagmail/voice/MainActivity.java
+++ app/src/main/java/com/p14osheagmail/voice/MainActivity.java
@@ -1,13 +1,67 @@
 package com.p14osheagmail.voice;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView voiceToText;
+    private static final int REQ_CODE_VOICE_IN = 143;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        voiceToText = (TextView) findViewById(R.id.voiceIn);
+
+        ImageView speakPromt = (ImageView) findViewById(R.id.btnToSpeak);
+        speakPromt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startVoiceToTextService();
+            }
+        });
+
+    }
+
+    private void startVoiceToTextService() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        //You can set here own local Language.
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hiiii, Tell Me SomeThing");
+        try {
+            startActivityForResult(intent, REQ_CODE_VOICE_IN);
+        }
+        catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_VOICE_IN: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    voiceToText.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 }
